3ee56d915f1e2d2ab975749b884043d5179c969e
Maximilian <scientificresearch309@gmail.com>
2024-04-26 19:05:28 +0200

    this button +++ works well with call back function!

--- src/App.tsx
+++ src/App.tsx
@@ -81,6 +81,23 @@ function TabContent({ item }: { item: IContent | undefined }) {
   function handleInc() {
     setLikes(likes + 1);
   }
+  // const clonedArray = JSON.parse(JSON.stringify(nodesArray));
+
+  function handleTripleInc() {
+    // NOTE: below commands work due to the call back function and when i click on the +++ button, it will increase by three and not by one!
+    setLikes((like) => like + 1);
+    setLikes((like) => like + 1);
+    setLikes((like) => like + 1);
+    // NOTE: below commands don't work! when i click on the +++ button, it only increase by one and not by three!
+    // setLikes(() => likes + 1);
+    // setLikes(() => likes + 1);
+    // setLikes(() => likes + 1);
+  }
+
+  const handleUndo = () => {
+    setShowDetails(true);
+    setLikes(0);
+  };
 
   return (
     <div className="tab-content">
@@ -95,12 +112,12 @@ function TabContent({ item }: { item: IContent | undefined }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
